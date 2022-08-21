86e985b6a9fe653e626a375772e1d0b7f5a6f343
lucas bretzke <lucas.bretzke@warren.com.br>
2022-08-21 18:21:50 -0300

    added: tests

--- tests/unit/Car/Listing_OnCars.spec.ts
+++ tests/unit/Car/Listing_OnCars.spec.ts
@@ -1,11 +1,12 @@
 import { render, fireEvent, screen, waitFor, within } from '@testing-library/vue'
 import '@testing-library/jest-dom'
 import ListingOnCars from '@/components/ListingOnCars.vue'
-import { carArrayMock } from './ListingOnCarsMocks'
-import CarService from '@/services/carService'
+import { carArrayMock, putCarMock } from './ListingOnCarsMocks'
+import carService from '@/services/carService'
 
 jest.mock('@/services/carService', () => ({
   get: jest.fn().mockResolvedValue(require('./ListingOnCarsMocks.ts').carArrayMock),
+  put: jest.fn().mockResolvedValue({}),
 }))
 
 type Props = {
@@ -22,19 +23,21 @@ describe('ListingOnCars.vue', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
+
   it('Should render component default values', async () => {
-    jest.spyOn(CarService, 'get').mockResolvedValueOnce([]) // para testar quando retornar array vazio de carros
-    // jest.spyOn(CarService, 'get').mockRejectedValueOnce({}) para testar o catch do axios
+    jest.spyOn(carService, 'get').mockResolvedValueOnce([]) // para testar quando retornar array vazio de carros
     const { getByTestId } = renderListingOnCars()
 
     screen.getByText('Lista de carros')
     screen.getByText('Adicionar carro')
     screen.getByText('Carro')
-    screen.getAllByText('Ano')[0]
+    screen.getByText('Ano')
 
-    screen.getByText('Loading . . .')
     expect(getByTestId('modal-form')).not.toBeVisible()
     expect(getByTestId('confirmation-modal')).not.toBeVisible()
+    await waitFor(() => {
+      expect(screen.getByText('Erro')).toBeVisible()
+    })
   })
 
   it('should render cars rows', async () => {
@@ -56,10 +59,76 @@ describe('ListingOnCars.vue', () => {
       within(firstCarRow[3]).getByText('Deletar carro')
     })
     expect(screen.getByText('Loading . . .')).not.toBeVisible()
+    expect(screen.getByText('Erro')).not.toBeVisible()
   })
 
-  it('Should render component ConfirmationModal', async () => {
+  it('should give an alert error when rendering lines of cars', async () => {
+    jest.spyOn(carService, 'get').mockRejectedValueOnce({})
+    renderListingOnCars()
+
+    await waitFor(() => {
+      expect(screen.getByText('Erro')).toBeVisible()
+    })
+  })
+
+  it('Must render Modalform component with empty inputs', async () => {
     const { getByTestId } = renderListingOnCars()
+
+    await fireEvent.click(screen.getByText('Adicionar carro'))
+
+    expect(getByTestId('modal-form')).toBeVisible()
+
+    expect(screen.getByLabelText('inputName')).toHaveValue("")
+    expect(screen.getByLabelText('inputMarca')).toHaveValue("")
+    expect(screen.getByLabelText('inputCor')).toHaveValue("")
+    expect(screen.getByLabelText('inputAno')).toHaveValue("")
+    expect(screen.getByLabelText('inputPortas')).toHaveValue("")
+    expect(screen.getByLabelText('inputCV')).toHaveValue("")
+    expect(screen.getByLabelText('inputCâmbio')).toHaveValue("")
+    expect(screen.getByLabelText('inputAlarme')).toHaveValue("")
+    expect(screen.getByLabelText('inputTetoSolar')).toHaveValue("")
+    expect(screen.getByLabelText('inputComputadorDeBordo')).toHaveValue("")
+
+  })
+
+  it('Should render component ModalForm with empty filled out accordingly ', async () => {
+    jest.spyOn(carService, 'put').mockResolvedValueOnce(putCarMock)
+    renderListingOnCars()
+
+    await waitFor(async () => {
+      const carsList = screen.getAllByRole('list', {
+        name: 'cars-label'
+      })
+      const firstCarRow = within(carsList[0]).getAllByRole('listitem')
+      await fireEvent.click(within(firstCarRow[2]).getByText('Editar carro'))
+
+      expect(screen.getByTestId('modal-form')).toBeVisible()
+
+      expect(screen.getByLabelText('inputName')).toHaveValue('Fusca')
+      expect(screen.getByLabelText('inputMarca')).toHaveValue('volks')
+      expect(screen.getByLabelText('inputCor')).toHaveValue('branco')
+      expect(screen.getByLabelText('inputAno')).toHaveValue('1999')
+      expect(screen.getByLabelText('inputPortas')).toHaveValue('2')
+      expect(screen.getByLabelText('inputCV')).toHaveValue('70')
+      expect(screen.getByLabelText('inputCâmbio')).toHaveValue('manual')
+      expect(screen.getByLabelText('inputAlarme')).toHaveValue('não tem')
+      expect(screen.getByLabelText('inputTetoSolar')).toHaveValue('não tem')
+      expect(screen.getByLabelText('inputComputadorDeBordo')).toHaveValue('não tem')
+
+      await fireEvent.click(screen.getByText('CONCLUIR'))
+
+      expect(screen.getByText('Loading . . .')).toBeVisible()
+      expect(carService.put).toBeCalledWith(putCarMock)
+
+      await waitFor(() => {
+        expect(screen.getByText('Loading . . .')).not.toBeVisible()
+        expect(screen.getByTestId('modal-form')).not.toBeVisible()
+      })
+    })
+  })
+
+  it('should render component ConfirmationModal ', async () => {
+    renderListingOnCars()
 
     await waitFor(async () => {
       const carsList = screen.getAllByRole('list', {
@@ -68,7 +137,9 @@ describe('ListingOnCars.vue', () => {
       const firstCarRow = within(carsList[0]).getAllByRole('listitem')
       await fireEvent.click(within(firstCarRow[3]).getByText('Deletar carro'))
 
-      expect(getByTestId('confirmation-modal')).toBeVisible()
+      expect(screen.getByTestId('confirmation-modal')).toBeVisible()
+      await fireEvent.click(screen.getByText('Cancelar'))
+      expect(screen.getByTestId('confirmation-modal')).not.toBeVisible()
     })
   })
 })
